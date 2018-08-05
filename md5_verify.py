667009bf4c256d2c6969ce3cdd5eb2fd4fd667c6
Fazx <617477259@qq.com>
2018-08-05 22:55:20 +0800

    对header的分割操作

--- md5_verify.py
+++ md5_verify.py
@@ -1,7 +1,7 @@
 #-*-coding:utf-8-*-
 import hashlib
 
-flag = "5273e2"
+flag = "b66b9d"
 
 def MD5(data):
 	return hashlib.md5(data).hexdigest()
@@ -10,6 +10,6 @@ def MD5(data):
 for i in range(1000000):
 	st = str(i)
 	md5 = MD5(st)
-	if(md5[0:6] == flag):
+	if(md5[-6:] == flag):
 		print(st, md5)
 		break

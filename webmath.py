fe45231af6c341a64f98b332538e3c49f5f416a8
Fazx <617477259@qq.com>
2018-08-05 22:11:14 +0800

    网页中的四则运算_正则

---
+++ webmath.py
@@ -0,0 +1,11 @@
+import requests
+import re
+
+url = 'http://120.24.86.145:8002/qiumingshan/'
+s = requests.Session()
+source = s.get(url)
+expression = re.search(r'(\d+[+\-*])+(\d+)',source.text).group()
+result = eval(expression)
+post = {'value':result}
+r = s.post(url,data = post)
+print(r.content)

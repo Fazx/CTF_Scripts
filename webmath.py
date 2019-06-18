419b63593ac2f4c1f1d944892e6a33a8182b1acc
Fazx <617477259@qq.com>
2019-06-18 21:41:15 +0800

    Web表达式计算

--- webmath.py
+++ webmath.py
@@ -9,3 +9,17 @@ result = eval(expression)
 post = {'value':result}
 r = s.post(url,data = post)
 print(r.content)
+
+
+# import requests,re
+# s = requests.Session()
+
+# url = 'http://lab1.xseclab.com/xss2_0d557e6d2a4ac08b749b61473a075be1/index.php'
+# html = s.get(url).content
+
+# reg = r'([0-9].+)=<'
+# pattern = re.compile(reg)
+# match = re.findall(pattern,html)
+
+# payload = {'v': eval(match[0])}
+# print s.post(url, data=payload).content
